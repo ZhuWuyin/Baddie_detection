8a6d741ec1a7d3ad4889cf4f737aea4b600794d2
ZhuWuyin <70063950+ZhuWuyin@users.noreply.github.com>
2024-02-06 03:09:34 -0500

    Add files via upload

---
+++ Baddie_detection/detect.py
@@ -0,0 +1,69 @@
+import cv2, pyautogui, time, keyboard, datetime, os
+
+def getVelocity():
+    start=pyautogui.position()
+    time.sleep(0.01)
+    end=pyautogui.position()
+
+    Vx=(start[0]-end[0])/0.01
+    Vy=(start[1]-end[1])/0.01
+
+    return (Vx, Vy)
+
+def check_keys_except(excluded_keys):
+    all_keys = set([chr(i) for i in range(32, 127) if chr(i) not in excluded_keys]) | keyboard.all_modifiers
+
+    for key in all_keys:
+        if keyboard.is_pressed(key):
+            return True
+        
+    return False
+
+capture=cv2.VideoCapture(0)
+start=False
+add=False
+
+start_hotkey = input("start hotkey: ")
+end_hotkey = input("end hotkey: ")
+
+video_start=0
+countdown_time=5
+
+if not os.path.exists(os.getcwd()+"\\video"):
+    os.makedirs("video")
+
+file_index=0
+fourcc=cv2.VideoWriter_fourcc(*"mp4v")
+out=cv2.VideoWriter("video/video{0}.mp4".format(file_index), fourcc, 20.0, (640, 480))
+
+excluded_keys=[start_hotkey, end_hotkey]
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+font_size = 0.5
+font_color = (255, 255, 255)
+
+print("ok")
+while True:
+    Vcurr=getVelocity()
+
+    if keyboard.is_pressed(start_hotkey) and not start:
+        start=True
+    if keyboard.is_pressed(end_hotkey) and start:
+        start=False
+
+    if start and ((Vcurr[0]!=0 or Vcurr[1]!=0) or check_keys_except(excluded_keys)):
+        video_start=int(time.time())
+        add=True
+    
+    if time.time()<=video_start+countdown_time:
+        ret, frame=capture.read()
+        if not ret:
+            continue
+        frame=cv2.flip(frame, 1)
+        text = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        cv2.putText(frame, text, (10, 20), font, font_size, font_color, 1, cv2.LINE_AA)
+        out.write(frame)
+    elif add:
+        file_index+=1
+        add=False
+        out=cv2.VideoWriter("video/video{0}.mp4".format(file_index), fourcc, 20.0, (640, 480))
